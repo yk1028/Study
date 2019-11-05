b4e17db90bd94afefd8a945021137aef19ddc4ec
yk1028 <wongeunsong94@gmail.com>
2019-11-05 13:47:59 +0900

    feat: NotificationService 추가
    - 서버에서 독자적으로 메세지를 보낼 때 사용

---
+++ websocket/src/main/java/com/yk1028/websocket/controller/NotificationApiController.java
@@ -0,0 +1,21 @@
+package com.yk1028.websocket.controller;
+
+import com.yk1028.websocket.service.NotificationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationApiController {
+
+    private NotificationService notificationService;
+
+    public NotificationApiController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping("/noti/{message}")
+    public void sendNotification(@PathVariable String message) {
+        notificationService.send(message);
+    }
+}
