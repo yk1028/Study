1141eb4a92c1cf798ba5b7657fbce97c2243cc55
yk1028 <wongeunsong94@gmail.com>
2021-01-23 17:51:49 +0900

    feat: helloworld

--- api/README.md
+++ api/README.md
@@ -1,2 +1,5 @@
 https://daddyprogrammer.org/post/19/spring-boot2-start-intellij/
-- 위 블로그를 바탕으로 kotlin으로 변환
+-  블로그를 바탕으로 kotlin으로 변환
+
+### kotlin으로 작성하면서 변경사항
+- freemarker -> thymeleaf : freemarker가 잘 동작하지 않아 thymeleaf로 변경
--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -16,7 +16,13 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	
+	implementation("com.h2database:h2")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
